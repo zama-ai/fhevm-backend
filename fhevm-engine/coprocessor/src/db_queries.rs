686d81da9fa1cadfe13de742b2bfa0ff65a123ac
James Marsh <james.marsh@optalysys.com>
2025-01-31 09:03:42 +0000

    Update tfhe-rs version and fix build

--- fhevm-engine/coprocessor/src/db_queries.rs
+++ fhevm-engine/coprocessor/src/db_queries.rs
@@ -110,7 +110,7 @@ pub struct FetchTenantKeyResult {
     pub verifying_contract_address: String,
     pub acl_contract_address: String,
     pub server_key: tfhe::ServerKey,
-    pub public_params: Arc<tfhe::zk::CompactPkePublicParams>,
+    pub public_params: Arc<tfhe::zk::CompactPkeCrs>,
 }
 
 /// Returns chain id and verifying contract address for EIP712 signature and tfhe server key
@@ -165,7 +165,7 @@ where
             .expect("We can't deserialize our own validated sks key");
         let pks: tfhe::CompactPublicKey = safe_deserialize_key(&key.pks_key)
             .expect("We can't deserialize our own validated pks key");
-        let public_params: tfhe::zk::CompactPkePublicParams =
+        let public_params: tfhe::zk::CompactPkeCrs =
             safe_deserialize_key(&key.public_params)
                 .expect("We can't deserialize our own validated public params");
         res.push(TfheTenantKeys {
